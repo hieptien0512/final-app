266699787576c5f73d2b46f481cd10dea912f747
hieptien0512 <hieptien0512@gmail.com>
2021-04-19 01:15:49 +0700

    share code on github

---
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -0,0 +1,124 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CheckoutStep from "../components/CheckoutStep";
+
+export default function PlaceOrderScreen(props) {
+  const cart = useSelector((state) => state.cart);
+  if (!cart.paymentMethod) {
+    props.history.push("/payment");
+  }
+  const toPrice = (num) => Number(num.toFixed(2));
+  cart.itemPrice = toPrice(
+    cart.cartItems.reduce((a, c) => a + c.qty * c.price, 0)
+  );
+  cart.shippingPrice = cart.itemPrice > 100 ? toPrice(0) : toPrice(10);
+  cart.taxPrice = toPrice(0.15 * cart.itemPrice);
+  cart.totalPrice = cart.itemPrice + cart.shippingPrice + cart.taxPrice;
+
+  const placeOrderHandler = () => {};
+  return (
+    <div>
+      <CheckoutStep step1 step2 step3 step4></CheckoutStep>
+      <div className="row top">
+        <div className="col-2">
+          <ul>
+            <li>
+              <div className="card card-body">
+                <h2>Shipping</h2>
+                <p>
+                  <strong>Name:</strong> {cart.shippingAddress.fullName} <br />
+                  <strong>Address:</strong>
+                  {cart.shippingAddress.address}, {cart.shippingAddress.city}
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="card card-body">
+                <h2>Payment</h2>
+                <p>
+                  <strong>Method:</strong> {cart.paymentMethod}
+                </p>
+              </div>
+            </li>
+            <li>
+              <div className="card card-body">
+                <h2>Oder Item</h2>
+                <ul>
+                  {cart.cartItems.map((item) => (
+                    <li key={item.product}>
+                      <div className="row">
+                        <div>
+                          <img
+                            src={item.image}
+                            alt={item.name}
+                            className="small"
+                          ></img>
+                        </div>
+                        <div className="min-30">
+                          <Link to={`/product/${item.product}`}>
+                            {item.name}{" "}
+                          </Link>
+                        </div>
+                        <div>
+                          {item.qty} x ${item.price} = ${item.qty * item.price}
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div className="col-1">
+          <div className="card card-body">
+            <ul>
+              <li>
+                <h2>Order Summary</h2>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Items</div>
+                  <div>${cart.itemPrice}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Shipping</div>
+                  <div>${cart.shippingPrice}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>Tax</div>
+                  <div>${cart.taxPrice}</div>
+                </div>
+              </li>
+              <li>
+                <div className="row">
+                  <div>
+                    <strong>Order Total</strong>
+                  </div>
+                  <div>
+                    <strong>${cart.totalPrice}</strong>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <button
+                  type="button"
+                  onClick={placeOrderHandler}
+                  className="primary block"
+                  disabled={cart.cartItems.length === 0}
+                >
+                  Place order
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
